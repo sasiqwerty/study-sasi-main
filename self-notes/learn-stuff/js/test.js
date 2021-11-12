aeb21315b32cfa0e5eaf81b9fbead4a582d2b87f
sasiqwerty <sarathsasi.g@gmail.com>
2021-11-12 18:53:34 +0530

    case

--- self-notes/learn-stuff/js/test.js
+++ self-notes/learn-stuff/js/test.js
@@ -1,4 +1,10 @@
-console.log("what am i doing");
-let stringIdea = 12;
-console.log(`what is a string at the end the day  ${stringIdea}`);
-console.log(stringIdea);
+body1 = document.getElementById("body-1");
+for (let i = 1; i < 74; i++) {
+  text = `https://image.slidesharecdn.com/pharmacovigilanceprocessworkflow-katalysthls-170111211147/95/pharmacovigilance-process-work-flow-katalyst-hls-${i}-1024.jpg?cb=1484169559`;
+  a = document.createElement("a");
+  link = document.createTextNode(i + `         `);
+  a.appendChild(link);
+  a.href = text;
+  a.target = "_blank";
+  body1.appendChild(a);
+}
---
+++ self-notes/learn-stuff/phv/training/md files/training-guide/valid-case.md
@@ -0,0 +1,2 @@
+https://image.slidesharecdn.com/pharmacovigilanceprocessworkflow-katalysthls-170111211147/95/pharmacovigilance-process-work-flow-katalyst-hls-1-1024.jpg?cb=1484169559
+
